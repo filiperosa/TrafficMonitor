67929b8864ccf53f2122ed6be79d88b2a51d5dde
Filipe Mendes Rosa <filipe.rosa@cern.ch>
2023-05-08 22:15:01 +0100

    script input file arg is now optional, if not passed it reads from stdin. Added high_traffic global flag and implemented alerts. Added global zero_timestamp to print relative time instead of timestamps. Added stats print of last chunk of logs.

--- traffic_monitor/monitor.py
+++ traffic_monitor/monitor.py
@@ -1,9 +1,8 @@
 #
-
+import sys
 import argparse
 from typing import List
 from traffic_monitor.log import Log
-from datetime import datetime, timedelta
 
 # Buffer to store logs from last 2 minutes
 log_buffer: List[Log] = []
@@ -11,33 +10,48 @@ log_buffer: List[Log] = []
 # Chunk of X seconds of logs (default 10 seconds)
 log_chunk: List[Log] = []
 
+# High traffic flag
+high_traffic = False
+
+# Timesamp of first log
+zero_timestamp = None
+
 def get_args() -> argparse.Namespace:
     """Parse command line arguments"""
 
-    parser = argparse.ArgumentParser(description='HTTP traffic monitor from stdin or csv file')
-    parser.add_argument('requests', type=str, help='HTTP requests', nargs='?')
-    parser.add_argument('--file', type=str, help='CSV file path with HTTP requests')
+    parser = argparse.ArgumentParser(description='HTTP traffic monitor from a csv file or stdin')
+    # parser.add_argument('requests', type=str, help='HTTP requests', nargs='*')
+    parser.add_argument('input_file', type=argparse.FileType('r'), default=sys.stdin, nargs="?")
     parser.add_argument('--threshold', type=str, help='Expected average requests per second', default=10)
-    
     parser.add_argument('--timeout', type=int, help='Time limit to wait for new logs', default=60)
     parser.add_argument('--chunksize', type=int, help='Duration of log chunks used for printing stats, default is 10 seconds', default=10)
     parser.add_argument('--window', type=int, help='Time window of logs kept in memory, default is 2 minutes', default=2)
 
-    
-    return parser.parse_args()
+    try:
+        return parser.parse_args()
+    except Exception as __:
+        parser.print_help()
+        exit(1)
+
 
 def monitor():
     """Iterate over logs and compute stats and alerts"""
 
+    global zero_timestamp
     args = get_args()
 
-    with open(args.file, 'r') as f:
+    with args.input_file as f:
+        # Skip header
         f.readline()
+
         for line in f:
             values = [eval(value) for value in line.split(',')]
             log = Log(*values)
 
-            # Remove logs older than 2 minutes from buffer
+            if not zero_timestamp:
+                zero_timestamp = log.timestamp
+
+            # Remove logs older than window minutes
             while(len(log_buffer) and (log.timestamp - log_buffer[0].timestamp) > args.window*60):
                 log_buffer.pop(0)
             log_buffer.append(log)
@@ -47,12 +61,18 @@ def monitor():
                 stats(log_chunk)
                 log_chunk.clear()
             log_chunk.append(log)
-    # print(logs)
+
+            # Compute alerts
+            alerts(log_buffer, args.threshold)
+    
+    # Print stats for last chunk
+    stats(log_chunk)
 
 
 def stats(logs: List[Log]):
     """Compute stats for a list of logs"""
 
+    global zero_timestamp
     sections = {}
     request_types = {}
     total_requests = len(logs)
@@ -80,9 +100,9 @@ def stats(logs: List[Log]):
     # Sort request types by hits
     request_types = {k: v for k, v in sorted(request_types.items(), key=lambda item: item[1], reverse=True)}
 
-    first_time = logs[0].timestamp
-    last_time = logs[len(logs)-1].timestamp
-    print(f"Stats from {first_time} to {last_time} ({last_time - first_time} seconds)")
+    first_time = logs[0].timestamp - zero_timestamp
+    last_time = logs[len(logs)-1].timestamp - zero_timestamp
+    print(f"Stats from seconds {first_time} to {last_time} ({last_time - first_time} seconds)")
 
     print(f"Total requests: {total_requests}")
     print(f"Total bytes: {total_bytes}")
@@ -98,18 +118,31 @@ def stats(logs: List[Log]):
     print()
     
 
-def alerts(logs: List[Log]):
+def alerts(logs: List[Log], threshold: int):
     """Compute alerts for a list of logs"""
+    
+    global high_traffic
+    global zero_timestamp
 
-    # Compute average requests per second
-    # If average is above threshold, print alert
-    # If average is below threshold, print recovery
-    # If average is above threshold for more than 2 minutes, print high traffic alert
+    # If there are no logs at different times, return
+    first_time = logs[0].timestamp
+    last_time = logs[len(logs)-1].timestamp
+    if last_time == first_time:
+        return
 
-    # Whenever total traffic for the past 2 minutes exceeds a certain number on average, print a message to the console saying that “High traffic generated an alert - hits = {value}, triggered at {time}”. The default threshold should be 10 requests per second but should be configurable
+    exact_duration = last_time - first_time
+    requests_per_second = len(logs)/exact_duration
+    # print(f"requests_per_second: {requests_per_second}")
 
-    pass
-
+    # Requests per second is above threshold
+    if(not high_traffic and requests_per_second > threshold):
+        print(f"High traffic generated an alert - hits = {len(logs)}, triggered at {logs[len(logs) - 1].timestamp - zero_timestamp} seconds\n")
+        high_traffic = True
+    
+    # Requests per second is back below threshold
+    elif(high_traffic and requests_per_second <= threshold):
+        print(f"High traffic alert recovered at {logs[len(logs) - 1].timestamp - zero_timestamp} seconds\n")
+        high_traffic = False
 
 
 
